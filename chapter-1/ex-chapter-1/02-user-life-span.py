36ebe45a09776d3a7e5d0c103572a79f106a968a
Danilo Quattrini <daniloquattrini.com@gmail.com>
2025-09-19 23:32:52 +0200

    feat: complete exercise about the life span in second

--- chapter-1/ex-chapter-1/02-user-life-span.py
+++ chapter-1/ex-chapter-1/02-user-life-span.py
@@ -4,4 +4,8 @@
     the number of seconds a person can live. Assume a person can live hundred years
 """
 
-years_to_live = int(input("Enter number of years you have lived:"))
+years_to_live = int(input("Enter number of years you have lived: "))
+
+years_into_second = 365 * 86400
+
+print(f"You have lived for {years_to_live * years_into_second} seconds.")
